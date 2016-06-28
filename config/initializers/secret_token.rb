6daef5a411d8a60d5c0b2a811f3cb2abf3ec493c
Your Name <zberms@gmail.com>
2016-06-28 15:54:22 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoicChess::Application.config.secret_key_base = 'b02af4ed78836e0a007c890e7f38df540669ad77d076cffe3899647bfbfd000e5e5080fc5841fff22763a5ce871d3f4f077b4c2693f964cdd67c0da25d6fad29'
