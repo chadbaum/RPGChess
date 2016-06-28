a68e7df38366bfc036233832379cfe495f976ce4
Aleks Gorbenko <kucher12@gmail.com>
2016-06-28 18:40:01 +0000

    first test passed

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe GamesController, type: :controller do
   describe "games#index" do
     it "should successfully load the homepage" do
       get :index
-      expect(response).to have_http_request(:success)
+      expect(response).to have_http_status(:success)
     end
   end
 end
