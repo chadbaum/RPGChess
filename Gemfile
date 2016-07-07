6d5a8304f160c60752bd9396386fb40d7eb454d7
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-07-07 17:03:59 +0000

    added capybara gem

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails_12factor', group: :production
 gem 'simple_form'
 gem 'devise'
+gem 'capybara'
 gem 'bootstrap-sass'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+include Warden::Test::Helpers
+
+describe "log in" do
+  it "allows new users to register with an email address and password" do
+    before(:each) do
+      @user = Factory.create(:user)
+      @user.confirm!
+      login_as @user, :scope => :user
+    end
+  end
+
+end
