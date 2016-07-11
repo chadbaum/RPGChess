a40b5f3c62cc7da5327d11c8149400b91aad1ea3
Chad Baum <chadbaum@gmail.com>
2016-07-11 19:20:32 +0000

    Added rspec tests for white and black king move validation

--- spec/factories/kings.rb
+++ spec/factories/kings.rb
@@ -1,7 +1,15 @@
 FactoryGirl.define do
   factory :king do
-    color 'white'
-    x_position 7
-    y_position 3
+
+    trait :white do
+      color 'white'
+      x_position 7
+      y_position 3
+    end
+    trait :black do
+      color 'black'
+      x_position 0
+      y_position 3
+    end
   end
 end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe King, type: :model do
+  describe "white king movement validation" do
 
+    it "should return false if the king is not being moved from its original location" do
+      king = FactoryGirl.create(:king, :white)
+      expect(king.valid_move?(7,3)).to eq false
+    end
+
+    it "should return false if the king moves 2 spaces to the right" do
+      king = FactoryGirl.create(:king, :white)
+      expect(king.valid_move?(7,5)).to eq false
+    end
+
+    it "should return false if the king moves 3 spaces diagonally forward and to the right" do
+      king = FactoryGirl.create(:king, :white)
+      expect(king.valid_move?(4,6)).to eq false
+    end
+
+    it "should return true if the king moves forward and to the right 1 space" do
+      king = FactoryGirl.create(:king, :white)
+      expect(king.valid_move?(6,4)).to eq true
+    end
+
+    it "should return true if the king moves to the left 1 space" do
+      king = FactoryGirl.create(:king, :white)
+      expect(king.valid_move?(7,2)).to eq true
+    end
+
+    it "should return true if the king moves forward 1 space" do
+      king = FactoryGirl.create(:king, :white)
+      expect(king.valid_move?(6,3)).to eq true
+    end
+
+  end
+
+  describe "black king movement validation" do
+
+    it "should return false if the king is not being moved from its original location" do
+      king = FactoryGirl.create(:king, :black)
+      expect(king.valid_move?(0,3)).to eq false
+    end
+
+    it "should return false if the king moves 2 spaces to the right" do
+      king = FactoryGirl.create(:king, :black)
+      expect(king.valid_move?(0,5)).to eq false
+    end
+
+    it "should return false if the king moves 3 spaces diagonally forward and to the right" do
+      king = FactoryGirl.create(:king, :black)
+      expect(king.valid_move?(3,6)).to eq false
+    end
+
+    it "should return true if the king moves forward and to the right 1 space" do
+      king = FactoryGirl.create(:king, :black)
+      expect(king.valid_move?(1,4)).to eq true
+    end
+
+    it "should return true if the king moves to the left 1 space" do
+      king = FactoryGirl.create(:king, :black)
+      expect(king.valid_move?(0,2)).to eq true
+    end
+
+    it "should return true if the king moves forward 1 space" do
+      king = FactoryGirl.create(:king, :black)
+      expect(king.valid_move?(1,3)).to eq true
+    end
+
+  end
 end
