845a0a2bd40393b4797604356f176655418248f0
George Fourikis <georgefourikis@hotmail.gr>
2016-07-12 17:44:49 +0000

    some working tests for review by team

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 require 'rails_helper'
 
+include Warden::Test::Helpers
+Warden.test_mode!
+
 describe 'home page' do
   it 'welcomes the user' do
     visit '/'
@@ -31,7 +34,23 @@ describe "security" do
 
     click_button "Log in"
 
+    # response.body.should include("Signed in successfully.")
     # page.should have_content('Signed in successfully.')
     # expect(page).to have_content('Signed in successfully.')
   end
 end
+
+
+
+
+describe "the signin process", :type => :feature do
+
+  it "signs me in" do
+    visit '/users/sign_in'
+    fill_in 'Email', :with => 'user@example.com'
+    fill_in 'Password', :with => 'password'
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully.'
+  end
+end
+
