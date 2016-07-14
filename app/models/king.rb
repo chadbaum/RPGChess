347baa5d7046a02b974c7aacf7bab73db6a623b2
Chad Baum <chadbaum@gmail.com>
2016-07-14 00:06:48 +0000

    Refactored comments from inline to block for king model

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,16 +1,27 @@
 class King < Piece
 
-  def valid_move?(x, y) #does not include "castling" logic yet or check logic
+  # Returns true if the provided x-y coordinates are not the
+  # same as the piece's initial x-y coordinates and if the
+  # coordinates represent a valid radial move.  Capture, collision,
+  # check, and checkmate logic are not implemented yet and thus ignored.
+  def valid_move?(x, y)
     moved?(x,y) && radial_move?(x,y)
   end
 
   private
 
-  def first_move? #will be used for "castling" logic, needs a database boolean check instead, this method will not work if king moves back into original spot
+  # Returns true if the king is in its original location.
+  # Note that this method will have to be replaced with a
+  # boolean flag set in database to support castling properly
+  # as it will improperly return true if king has moved out
+  # of its initial position and back in.
+  def first_move?
     x_position == 7 && y_position == 3 if color == "white"
     x_position == 0 && y_position == 3 if color == "black"
   end
 
+  # Returns true if the provided x-y coordinates are within
+  # 1 space of the king in any direction.
   def radial_move?(x,y)
     distance(x,"x") <= 1 && distance(y,"y") <= 1
   end
