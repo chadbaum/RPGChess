25a62b01f93d22c11fc7b62ad040eb52e8f03606
George Fourikis <georgefourikis@hotmail.gr>
2016-07-18 19:26:29 +0000

    minor fixes on schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -51,8 +52,10 @@ ActiveRecord::Schema.define(version: 20160712160707) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+  end
+
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
+  
 
 end
