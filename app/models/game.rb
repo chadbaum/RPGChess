810b26760ac2c764c9faf64073175bbfa9205be8
George Fourikis <georgefourikis@hotmail.gr>
2016-07-18 19:15:43 +0000

    changed name on creation on game.rb to pieces.create and then different types of pieces on each one

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,41 +9,42 @@ class Game < ActiveRecord::Base
 
     # Black Pieces of the board
     (0..7).each do |i|
-      Pawn.create(game_id: id, x_position: i, y_position: 1, color: "black")
+      Piece.create(type: 'Pawn', game_id: id, x_position: i, y_position: 1, color: "black")
     end
 
-    Rook.create(game_id: id, x_position: 0, y_position: 0, color: "black")
-    Rook.create(game_id: id, x_position: 7, y_position: 0, color: "black")
+    Piece.create(type: 'Rook',game_id: id, x_position: 0, y_position: 0, color: "black")
+    Piece.create(type: 'Rook',game_id: id, x_position: 7, y_position: 0, color: "black")
 
-    Knight.create(game_id: id, x_position: 1, y_position: 0, color: "black")
-    Knight.create(game_id: id, x_position: 6, y_position: 0, color: "black")
+    Piece.create(type: 'Knight',game_id: id, x_position: 1, y_position: 0, color: "black")
+    Piece.create(type: 'Knight',game_id: id, x_position: 6, y_position: 0, color: "black")
 
-    Bishop.create(game_id: id, x_position: 2, y_position: 0, color: "black")
-    Bishop.create(game_id: id, x_position: 5, y_position: 0, color: "black")
+    Piece.create(type: 'Bishop',game_id: id, x_position: 2, y_position: 0, color: "black")
+    Piece.create(type: 'Bishop',game_id: id, x_position: 5, y_position: 0, color: "black")
 
-    Queen.create(game_id: id, x_position: 3, y_position: 0, color: "black")
-    King.create(game_id: id, x_position: 4, y_position: 0, color: "black")
+    Piece.create(type: 'Queen',game_id: id, x_position: 3, y_position: 0, color: "black")
+    Piece.create(type: 'King',game_id: id, x_position: 4, y_position: 0, color: "black")
 
 
     # White Pieces of the board
     (0..7).each do |i|
-      Pawn.create(game_id: id, x_position: i, y_position: 6, color: "white")
+      Piece.create(type: 'Pawn',game_id: id, x_position: i, y_position: 6, color: "white")
     end
 
-    Rook.create(game_id: id, x_position: 0, y_position: 7, color: "white")
-    Rook.create(game_id: id, x_position: 7, y_position: 7, color: "white")
+    Piece.create(type: 'Rook',game_id: id, x_position: 0, y_position: 7, color: "white")
+    Piece.create(type: 'Rook',game_id: id, x_position: 7, y_position: 7, color: "white")
 
-    Knight.create(game_id: id, x_position: 1, y_position: 7, color: "white")
-    Knight.create(game_id: id, x_position: 6, y_position: 7, color: "white")
+    Piece.create(type: 'Knight',game_id: id, x_position: 1, y_position: 7, color: "white")
+    Piece.create(type: 'Knight',game_id: id, x_position: 6, y_position: 7, color: "white")
     
-    Bishop.create(game_id: id, x_position: 2, y_position: 7, color: "white")
-    Bishop.create(game_id: id, x_position: 5, y_position: 7, color: "white")
+    Piece.create(type: 'Bishop',game_id: id, x_position: 2, y_position: 7, color: "white")
+    Piece.create(type: 'Bishop',game_id: id, x_position: 5, y_position: 7, color: "white")
     
-    Queen.create(game_id: id, x_position: 3, y_position: 7, color: "white")
-    King.create(game_id: id, x_position: 4, y_position: 7, color: "white")
+    Piece.create(type: 'Queen',game_id: id, x_position: 3, y_position: 7, color: "white")
+    Piece.create(type: 'King',game_id: id, x_position: 4, y_position: 7, color: "white")
 
 
   end
 
   
 end
+
