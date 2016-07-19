910235440f148f5ac3cccea3ccf407664fb2333a
Chad Baum <chadbaum@gmail.com>
2016-07-19 19:00:52 +0000

    Pawn rspecs updated to Rubocop compliance

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -2,99 +2,100 @@ require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
   describe 'white pawn movement validation' do
-    it 'should return false if the pawn is not being moved from its original location' do
+    it 'should return false if not moved' do
       pawn = FactoryGirl.create(:pawn, :white)
       expect(pawn.valid_move?(5, 6)).to eq false
     end
 
-    it 'should return false if the pawn is moved 3 spaces to the left' do
+    it 'should return false if moved 3 spaces left' do
       pawn = FactoryGirl.create(:pawn, :white)
       expect(pawn.valid_move?(2, 6)).to eq false
     end
 
-    it 'should return false if the pawn is moved backwards' do
+    it 'should return false if moved 1 space backward' do
       pawn = FactoryGirl.create(:pawn, :white)
       expect(pawn.valid_move?(5, 7)).to eq false
     end
 
-    it 'should return true if the pawn is moved one space forward' do
+    it 'should return true if moved 1 space forward' do
       pawn = FactoryGirl.create(:pawn, :white, :moved)
       expect(pawn.valid_move?(5, 4)).to eq true
     end
 
-    it 'should return false if the pawn is moved two spaces but not on its first move' do
+    it 'should return false if moved 2 spaces not on first move' do
       pawn = FactoryGirl.create(:pawn, :white, :moved)
       expect(pawn.valid_move?(5, 3)).to eq false
     end
 
-    it 'should return true if the pawn is moved two spaces forward on its first move' do
+    it 'should return true if moved 2 spaces forward on first move' do
       pawn = FactoryGirl.create(:pawn, :white)
       expect(pawn.valid_move?(5, 4)).to eq true
     end
 
-    it 'should return true if the pawn is moved one space forward on its first move' do
+    it 'should return true if moved 1 space forward on first move' do
       pawn = FactoryGirl.create(:pawn, :white)
       expect(pawn.valid_move?(5, 5)).to eq true
     end
   end
 
   describe 'black pawn movement validation' do
-    it 'should return false if the pawn is not being moved from its original location' do
+    it 'should return false if not moved' do
       pawn = FactoryGirl.create(:pawn, :black)
       expect(pawn.valid_move?(5, 1)).to eq false
     end
 
-    it 'should return false if the pawn is moved 3 spaces to the left' do
+    it 'should return false if moved 3 spaces left' do
       pawn = FactoryGirl.create(:pawn, :black)
       expect(pawn.valid_move?(2, 1)).to eq false
     end
 
-    it 'should return false if the pawn is moved backwards' do
+    it 'should return false if moved 1 space backward' do
       pawn = FactoryGirl.create(:pawn, :black)
       expect(pawn.valid_move?(5, 0)).to eq false
     end
 
-    it 'should return true if the pawn is moved one space forward' do
+    it 'should return true if moved 1 space forward' do
       pawn = FactoryGirl.create(:pawn, :black, :moved)
       expect(pawn.valid_move?(5, 6)).to eq true
     end
 
-    it 'should return false if the pawn is moved two spaces but not on its first move' do
+    it 'should return false if moved 2 spaces forward not on first move' do
       pawn = FactoryGirl.create(:pawn, :black, :moved)
       expect(pawn.valid_move?(5, 5)).to eq false
     end
 
-    it 'should return true if the pawn is moved two spaces forward on its first move' do
+    it 'should return true if moved 2 spaces forward on first move' do
       pawn = FactoryGirl.create(:pawn, :black)
       expect(pawn.valid_move?(5, 3)).to eq true
     end
 
-    it 'should return true if the pawn is moved one space forward on its first move' do
+    it 'should return true if moved 1 space forward on first move' do
       pawn = FactoryGirl.create(:pawn, :black)
       expect(pawn.valid_move?(5, 2)).to eq true
     end
   end
 
   describe 'pawn creation validation' do
-    it 'should have a type of Pawn' do
+    it 'should create a white pawn' do
       r = FactoryGirl.create(:pawn, color: 'white')
       expect(r.type).to eq('Pawn')
     end
 
-    it 'should not be allowed to create a red pawn' do
-      expect { FactoryGirl.create(:pawn, color: 'red') }.to raise_error(ActiveRecord::RecordInvalid)
+    it 'should fail to create a red pawn' do
+      expect { FactoryGirl.create(:pawn, color: 'red') }.to
+      raise_error(ActiveRecord::RecordInvalid)
     end
   end
 
   describe 'pawn move method' do
-    it "should return true and update the pawn's position when it makes a valid move" do
+    it 'should return true and update position on valid move' do
       pawn = FactoryGirl.create(:pawn, :white)
       expect(pawn.move!(5, 5)).to eq true
       expect(pawn.x_position).to eq 5
       expect(pawn.y_position).to eq 5
     end
 
-    it "should return nil and not update the pawn's position when it makes an invalid move" do
+    it 'should return nil and not update position on invalid move' do
       pawn = FactoryGirl.create(:pawn, :black)
       expect(pawn.move!(5, 0)).to eq false
       expect(pawn.x_position).to eq 5
