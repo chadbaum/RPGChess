bc3132a3f8d5199b127666dc4882ffd684975348
Chad Baum <chadbaum@gmail.com>
2016-07-19 14:55:27 +0000

    Fixed all bugs

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -5,9 +5,9 @@ include Warden::Test::Helpers
 Warden.test_mode!
 
 describe 'home page' do
-  it 'welcomes the user' do
+  it 'should welcomes the user' do
     visit '/'
-    page.should have_content('StoicChess')
+    expect(page).to have_content('StoicChess')
   end
 end
 
@@ -16,7 +16,7 @@ describe "the signin process", :type => :feature do
     User.create(:email => 'user@example.com', :password => 'password')
   end
 
-  it "signs me in" do
+  it "should sign me in" do
     visit '/users/sign_in'
     fill_in 'Email', :with => 'user@example.com'
     fill_in 'Password', :with => 'password'
@@ -24,4 +24,3 @@ describe "the signin process", :type => :feature do
     expect(page).to have_content 'Signed in successfully.'
   end
 end
-
