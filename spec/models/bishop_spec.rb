85087e25f78f30be177214b4de62e209ad485866
Chad Baum <chadbaum@gmail.com>
2016-07-19 19:12:55 +0000

    Updated bishop to Rubocop compliance

--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -2,79 +2,80 @@ require 'rails_helper'
 
 RSpec.describe Bishop, type: :model do
   describe 'white bishop movement validation' do
-    it 'should return false if the bishop is not being moved from its original location' do
+    it 'should return false if not moved' do
       bishop = FactoryGirl.create(:bishop, :white)
       expect(bishop.valid_move?(2, 7)).to eq false
     end
 
-    it 'should return false if the bishop is moved 2 spaces to the left' do
+    it 'should return false if moved 2 spaces left' do
       bishop = FactoryGirl.create(:bishop, :white)
       expect(bishop.valid_move?(0, 7)).to eq false
     end
 
-    it 'should return false if the bishop is moved forwards one space' do
+    it 'should return false if moved 1 space forward' do
       bishop = FactoryGirl.create(:bishop, :white)
       expect(bishop.valid_move?(2, 6)).to eq false
     end
 
-    it 'should return true if the bishop 3 spaces diagonally forward and to the right' do
+    it 'should return true if moved 3 spaces forward and 3 spaces right' do
       bishop = FactoryGirl.create(:bishop, :white)
       expect(bishop.valid_move?(5, 4)).to eq true
     end
 
-    it 'should return true if the bishop 2 spaces diagonally forward and to the left' do
+    it 'should return true if moved 2 spaces forward and 2 spaces left' do
       bishop = FactoryGirl.create(:bishop, :white)
       expect(bishop.valid_move?(0, 5)).to eq true
     end
   end
 
   describe 'black bishop movement validation' do
-    it 'should return false if the bishop is not being moved from its original location' do
+    it 'should return false if not moved' do
       bishop = FactoryGirl.create(:bishop, :black)
       expect(bishop.valid_move?(2, 0)).to eq false
     end
 
-    it 'should return false if the bishop is moved 2 spaces to the right' do
+    it 'should return false if moved 2 spaces right' do
       bishop = FactoryGirl.create(:bishop, :black)
       expect(bishop.valid_move?(4, 0)).to eq false
     end
 
-    it 'should return false if the bishop is moved forwards one space' do
+    it 'should return false if moved 1 space forward' do
       bishop = FactoryGirl.create(:bishop, :black)
       expect(bishop.valid_move?(2, 1)).to eq false
     end
 
-    it 'should return true if the bishop 3 spaces diagonally forward and to the right' do
+    it 'should return true if moved 3 spaces forward and spaces right' do
       bishop = FactoryGirl.create(:bishop, :black)
       expect(bishop.valid_move?(5, 3)).to eq true
     end
 
-    it 'should return true if the bishop 2 spaces diagonally forward and to the left' do
+    it 'should return true if moved 2 spaces forward and 2 spaces left' do
       bishop = FactoryGirl.create(:bishop, :black)
       expect(bishop.valid_move?(0, 2)).to eq true
     end
   end
 
   describe 'bishop creation validation' do
-    it 'should have a type of Bishop' do
+    it 'should create a white bishop' do
       r = FactoryGirl.create(:bishop, color: 'white')
       expect(r.type).to eq('Bishop')
     end
 
-    it 'should not be allowed to create a red bishop' do
-      expect { FactoryGirl.create(:bishop, color: 'red') }.to raise_error(ActiveRecord::RecordInvalid)
+    it 'should fail to create a red bishop' do
+      expect { FactoryGirl.create(:bishop, color: 'red') }.to
+      raise_error(ActiveRecord::RecordInvalid)
     end
   end
 
   describe 'bishop move method' do
-    it "should return true and update the bishop's position when it makes a valid move" do
+    it 'should return true and update position on valid move' do
       bishop = FactoryGirl.create(:bishop, :white)
       expect(bishop.move!(3, 6)).to eq true
       expect(bishop.x_position).to eq 3
       expect(bishop.y_position).to eq 6
     end
 
-    it "should return nil and not update the bishop's position when it makes an invalid move" do
+    it 'should return nil and not update position on invalid move' do
       bishop = FactoryGirl.create(:bishop, :black)
       expect(bishop.move!(3, 0)).to eq false
       expect(bishop.x_position).to eq 2
