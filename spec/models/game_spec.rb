c55c53c321577bb56db39193bc0c3868aeed1d6e
George Fourikis <georgefourikis@hotmail.gr>
2016-07-20 15:28:46 +0000

    changed some lines in the tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,25 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+  g = FactoryGirl.create(:game)
   describe 'populate the board' do
     it 'should give us 32 pieces upon board population' do
-      g = FactoryGirl.create(:game)
       expect(g.pieces.count).to eq 32
     end
     it 'should give me the last x position of population' do
-      g = FactoryGirl.create(:game)
       expect(g.pieces.last.x_position).to eq 4
     end
     it 'should give me the last y position of population' do
-      g = FactoryGirl.create(:game)
       expect(g.pieces.last.y_position).to eq 0
     end
     it 'should give me the last piece of the population as the King' do
-      g = FactoryGirl.create(:game)
       expect(g.pieces.last.type).to eq 'King'
     end
     it 'should give me the last pieces color' do
-      g = FactoryGirl.create(:game)
       expect(g.pieces.last.color).to eq 'black'
     end
   end
