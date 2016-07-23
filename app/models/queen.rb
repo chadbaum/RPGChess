ac84d4a5387af23184be6d3492a902a86f2de160
Chad Baum <chadbaum@gmail.com>
2016-07-23 04:35:21 +0000

    Significant refactor of bishop rspec tests for clarity, coverage, and to update to new obstruction logic

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -5,9 +5,9 @@ class Queen < Piece
   def valid_move?(x, y)
     moved?(x, y) &&
       (
-        horizontal_move?(x, y) ||
-        vertical_move?(x, y) ||
-        diagonal_move?(x, y)
+        clear_horizontal_move?(x, y) ||
+        clear_vertical_move?(x, y) ||
+        clear_diagonal_move?(x, y)
       )
   end
 end
--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -1,55 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Bishop, type: :model do
-  describe 'white bishop movement validation' do
-    let(:bishop) { FactoryGirl.create(:bishop, :white) }
+  let(:game) { FactoryGirl.create(:game) }
+  let(:bishop) { game.pieces.find_by(type: 'Bishop', color: 'white', x_position: 2, y_position: 7) }
+  let(:moved_bishop) { game.pieces.create(type: 'Bishop', color: 'white', x_position: 3, y_position: 3) }
 
-    it 'should return false if not moved' do
-      expect(bishop.valid_move?(2, 7)).to eq false
-    end
-
-    it 'should return false if moved 2 spaces left' do
-      expect(bishop.valid_move?(0, 7)).to eq false
-    end
-
-    it 'should return false if moved 1 space forward' do
-      expect(bishop.valid_move?(2, 6)).to eq false
-    end
-
-    it 'should return true if moved 3 spaces forward and 3 spaces right' do
-      expect(bishop.valid_move?(5, 4)).to eq true
-    end
-
-    it 'should return true if moved 2 spaces forward and 2 spaces left' do
-      expect(bishop.valid_move?(0, 5)).to eq true
-    end
-  end
-
-  describe 'black bishop movement validation' do
-    let(:bishop) { FactoryGirl.create(:bishop, :black) }
-
-    it 'should return false if not moved' do
-      expect(bishop.valid_move?(2, 0)).to eq false
-    end
-
-    it 'should return false if moved 2 spaces right' do
-      expect(bishop.valid_move?(4, 0)).to eq false
-    end
-
-    it 'should return false if moved 1 space forward' do
-      expect(bishop.valid_move?(2, 1)).to eq false
-    end
-
-    it 'should return true if moved 3 spaces forward and spaces right' do
-      expect(bishop.valid_move?(5, 3)).to eq true
-    end
-
-    it 'should return true if moved 2 spaces forward and 2 spaces left' do
-      expect(bishop.valid_move?(0, 2)).to eq true
-    end
-  end
-
-  describe 'bishop creation validation' do
+  describe 'creation validation' do
     it 'should create a white bishop' do
       bishop = FactoryGirl.create(:bishop, color: 'white')
       expect(bishop.type).to eq('Bishop')
@@ -61,31 +17,25 @@ RSpec.describe Bishop, type: :model do
     end
   end
 
-  describe 'bishop move method' do
-    it 'should return true and update position on valid move' do
-      bishop = FactoryGirl.create(:bishop, :white)
-      expect(bishop.move!(3, 6)).to eq true
-      expect(bishop.x_position).to eq 3
-      expect(bishop.y_position).to eq 6
-    end
-
-    it 'should return nil and not update position on invalid move' do
-      bishop = FactoryGirl.create(:bishop, :black)
-      expect(bishop.move!(3, 0)).to eq false
+  describe 'moved detection' do
+    it 'should return false if not moved' do
+      expect(bishop.move!(2, 7)).to eq false
       expect(bishop.x_position).to eq 2
-      expect(bishop.y_position).to eq 0
+      expect(bishop.y_position).to eq 7
     end
   end
 
-  describe 'obstructed logic' do
-    let(:game) { FactoryGirl.create(:game) }
-    let(:bishop) { game.pieces.find_by(type: 'Bishop', color: 'white', x_position: 2, y_position: 7) }
-    let(:moved_bishop) { game.pieces.create(type: 'Bishop', color: 'white', x_position: 3, y_position: 3) }
+  describe 'non-obstructed move' do
+    it 'should return true and update position on non-obstructed move' do
+      expect(moved_bishop.move!(5, 5)).to eq true
+      expect(moved_bishop.x_position).to eq 5
+      expect(moved_bishop.y_position).to eq 5
+    end
 
-    it 'should return false and not update position on obstructed move' do
-      expect(bishop.move!(4, 5)).to eq false
-      expect(bishop.x_position).to eq 2
-      expect(bishop.y_position).to eq 7
+    it 'should return true and update position on non-obstructed move' do
+      expect(moved_bishop.move!(2, 2)).to eq true
+      expect(moved_bishop.x_position).to eq 2
+      expect(moved_bishop.y_position).to eq 2
     end
 
     it 'should return true and update position on non-obstructed move' do
@@ -93,5 +43,45 @@ RSpec.describe Bishop, type: :model do
       expect(moved_bishop.x_position).to eq 5
       expect(moved_bishop.y_position).to eq 5
     end
+
+    it 'should return true and update position on non-obstructed move' do
+      expect(moved_bishop.move!(2, 4)).to eq true
+      expect(moved_bishop.x_position).to eq 2
+      expect(moved_bishop.y_position).to eq 4
+    end
+
+    it 'should return true and update position on non-obstructed move' do
+      expect(moved_bishop.move!(4, 2)).to eq true
+      expect(moved_bishop.x_position).to eq 4
+      expect(moved_bishop.y_position).to eq 2
+    end
+
+  end
+
+  describe 'obstructed move' do
+    it 'should return false and not update position on obstructed move' do
+      expect(bishop.move!(4, 5)).to eq false
+      expect(bishop.x_position).to eq 2
+      expect(bishop.y_position).to eq 7
+    end
+
+    it 'should return false and not update position on obstructed move' do
+      expect(bishop.move!(6, 3)).to eq false
+      expect(bishop.x_position).to eq 2
+      expect(bishop.y_position).to eq 7
+    end
+
+    it 'should return false and not update position on obstructed move' do
+      expect(moved_bishop.move!(5, 0)).to eq false
+      expect(moved_bishop.x_position).to eq 3
+      expect(moved_bishop.y_position).to eq 3
+    end
+
+    it 'should return false and not update position on obstructed move' do
+      expect(moved_bishop.move!(6, 7)).to eq false
+      expect(moved_bishop.x_position).to eq 3
+      expect(moved_bishop.y_position).to eq 3
+    end
+
   end
 end
