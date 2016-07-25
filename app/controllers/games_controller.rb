c5a69a432b92214328728b2191871d18e35890c2
Your Name <zberms@gmail.com>
2016-07-25 18:46:15 +0000

    Setup games controller, routes, add authentication, add form to new.html

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 # Main controller for chess session for CRUD
 # logic for our app
 class GamesController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!, only: [:create, :new]
   def index
   end
 
@@ -11,5 +11,15 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.new
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  private
+
+  def game_params
+    params.require(:game)
   end
 end
