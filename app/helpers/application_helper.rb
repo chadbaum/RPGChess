8c051a211349632e2bcef3e7707387c11262502e
Your Name <zberms@gmail.com>
2016-07-26 04:50:01 +0000

    Fix rubocop offenses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 # helper methods for application controller
 module ApplicationHelper
+  # Following 4 methods added to add a devise route
+  # for registration on index.html.erb
   def resource_name
     :user
   end
 
-  def resource_class 
-     User 
+  def resource_class
+    User
   end
 
   def resource
