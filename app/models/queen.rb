7ba4cf1654561e9e96218733493d1476ff678878
Chad Baum <chadbaum@gmail.com>
2016-07-29 16:48:27 +0000

    Minor refactoring of queen

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -4,10 +4,10 @@ class Queen < Piece
   # implemented yet and thus ignored.
   def valid_move?(x, y)
     moved?(x, y) &&
-      (
-        clear_horizontal_move?(x, y) ||
-        clear_vertical_move?(x, y) ||
+      [
+        clear_horizontal_move?(x, y),
+        clear_vertical_move?(x, y),
         clear_diagonal_move?(x, y)
-      )
+      ].any?
   end
 end
