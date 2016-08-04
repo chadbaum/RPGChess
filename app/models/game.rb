9a4da2db47b8458e8b6dcbfac0ad69747338cfc8
Chad Baum <chadbaum@gmail.com>
2016-08-04 16:23:20 +0000

    Refactored away from callbacks in game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,8 +3,32 @@ class Game < ActiveRecord::Base
   has_many :pieces
   has_many :players
 
-  after_create :populate_bishops!, :populate_rooks!, :populate_pawns!,\
-               :populate_knights!, :populate_queens_kings!
+  def populate!
+    populate_pawns!
+    populate_knights!
+    populate_bishops!
+    populate_rooks!
+    populate_queens!
+    populate_kings!
+  end
+
+  private
+
+  def populate_pawns!
+    (0..7).each do |i|
+      pieces.create(type: 'Pawn', x_position: i, y_position: 1, color: 'black')
+    end
+    (0..7).each do |i|
+      pieces.create(type: 'Pawn', x_position: i, y_position: 6, color: 'white')
+    end
+  end
+
+  def populate_knights!
+    pieces.create(type: 'Knight', x_position: 1, y_position: 7, color: 'white')
+    pieces.create(type: 'Knight', x_position: 6, y_position: 7, color: 'white')
+    pieces.create(type: 'Knight', x_position: 1, y_position: 0, color: 'black')
+    pieces.create(type: 'Knight', x_position: 6, y_position: 0, color: 'black')
+  end
 
   def populate_bishops!
     pieces.create(type: 'Bishop', x_position: 2, y_position: 7, color: 'white')
@@ -20,26 +44,13 @@ class Game < ActiveRecord::Base
     pieces.create(type: 'Rook', x_position: 7, y_position: 0, color: 'black')
   end
 
-  def populate_knights!
-    pieces.create(type: 'Knight', x_position: 1, y_position: 7, color: 'white')
-    pieces.create(type: 'Knight', x_position: 6, y_position: 7, color: 'white')
-    pieces.create(type: 'Knight', x_position: 1, y_position: 0, color: 'black')
-    pieces.create(type: 'Knight', x_position: 6, y_position: 0, color: 'black')
-  end
-
-  def populate_pawns!
-    (0..7).each do |i|
-      pieces.create(type: 'Pawn', x_position: i, y_position: 1, color: 'black')
-    end
-    (0..7).each do |j|
-      pieces.create(type: 'Pawn', x_position: j, y_position: 6, color: 'white')
-    end
-  end
-
-  def populate_queens_kings!
+  def populate_queens!
     pieces.create(type: 'Queen', x_position: 3, y_position: 7, color: 'white')
-    pieces.create(type: 'King', x_position: 4, y_position: 7, color: 'white')
     pieces.create(type: 'Queen', x_position: 3, y_position: 0, color: 'black')
+  end
+
+  def populate_kings!
+    pieces.create(type: 'King', x_position: 4, y_position: 7, color: 'white')
     pieces.create(type: 'King', x_position: 4, y_position: 0, color: 'black')
   end
 end
