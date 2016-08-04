b8de6e01a073349fd3925492230ce2a932a3c7ec
Aleks Gorbenko <kucher12@gmail.com>
2016-08-04 17:41:45 +0000

    fixed bug in test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -8,16 +8,16 @@ RSpec.describe Game, type: :model do
       expect(game.pieces.count).to eq 32
     end
     it 'should give me the last x position of population' do
-      expect(g.pieces.last.x_position).to eq 7
+      expect(game.pieces.last.x_position).to eq 7
     end
     it 'should give me the last y position of population' do
-      expect(g.pieces.last.y_position).to eq 7
+      expect(game.pieces.last.y_position).to eq 7
     end
     it 'should give me the last piece of the population as the King' do
-      expect(g.pieces.last.type).to eq 'Rook'
+      expect(game.pieces.last.type).to eq 'Rook'
     end
     it 'should give me the last pieces color' do
-      expect(g.pieces.last.color).to eq 'white'
+      expect(game.pieces.last.color).to eq 'white'
     end
   end
 end
