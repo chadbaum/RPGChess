228380006555b85ddc516d1a14230d6e35e4018a
Aleks Gorbenko <kucher12@gmail.com>
2016-08-04 17:25:02 +0000

    rubocop fixes

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-
   describe 'games#index' do
     it 'should successfully load the homepage' do
       get :index, method: :post
@@ -50,44 +49,36 @@ RSpec.describe GamesController, type: :controller do
         y_position: 0
       )
     end
-
-    it 'should update the coords of the White Pawn if the move coords are valid' do
-
+    it 'should update White Pawn if move coords are valid' do
       patch :update, params: { id: game.id, piece_id: wht_pawn.id,\
-            x_position: 7, y_position: 5 }
+                               x_position: 7, y_position: 5 }
 
       expect(response).to have_http_status(:success)
       wht_pawn.reload
       expect(wht_pawn.x_position).to eq(7)
       expect(wht_pawn.y_position).to eq(5)
     end
-
-    it 'should update the coords of the Black Knight if the move coords are valid' do
-
+    it 'should update Black Knight if move coords are valid' do
       patch :update, params: { id: game.id, piece_id: blk_knight.id,\
-            x_position: 0, y_position: 2 }
+                               x_position: 0, y_position: 2 }
 
       expect(response).to have_http_status(:success)
       blk_knight.reload
       expect(blk_knight.x_position).to eq(0)
       expect(blk_knight.y_position).to eq(2)
     end
-
-    it 'should NOT update the coords of the White Pawn if the move coords are invalid' do
-
-      patch :update, params: { id: game.id, piece_id: white_pawn.id,\
-            x_position: 6, y_position: 5 }
+    it 'should NOT update White Pawn if move coords are invalid' do
+      patch :update, params: { id: game.id, piece_id: wht_pawn.id,\
+                               x_position: 6, y_position: 5 }
 
       expect(response).to have_http_status(:success)
       wht_pawn.reload
       expect(wht_pawn.x_position).to eq(7)
       expect(wht_pawn.y_position).to eq(6)
     end
-
-    it 'should NOT update the coords of the Black Knight the move coords are invalid' do
-
+    it 'should NOT update Black Knight move coords are invalid' do
       patch :update, params: { id: game.id, piece_id: blk_knight.id,\
-                              x_position: 1, y_position: 2 }
+                               x_position: 1, y_position: 2 }
 
       expect(response).to have_http_status(:success)
       blk_knight.reload
