2efbc95127f938fa52db67aa096be11c614fc787
Chad Baum <chadbaum@gmail.com>
2016-08-06 19:20:33 +0000

    Added callback to create white and black players on game creation and gave ownership of pieces to players on populate

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,6 +5,7 @@ class Game < ActiveRecord::Base
   has_many :pieces
   has_many :players
   has_many :users, through: :players
+  after_create :create_players!
 
   def populate!
     populate_left_black_half!
@@ -17,43 +18,56 @@ class Game < ActiveRecord::Base
 
   private
 
+  def create_players!
+    create_players(color: 'white')
+    create_players(color: 'black')
+  end
+
+  def white
+    players.find_by(color: 'white')
+  end
+
+  def black
+    players.find_by(color: 'black')
+  end
+
   def populate_left_black_half!
-    pieces.create(type: 'Rook', x_position: 0, y_position: 0, color: 'black')
-    pieces.create(type: 'Knight', x_position: 1, y_position: 0, color: 'black')
-    pieces.create(type: 'Bishop', x_position: 2, y_position: 0, color: 'black')
-    pieces.create(type: 'Queen', x_position: 3, y_position: 0, color: 'black')
+    black.pieces.create(type: 'Rook', x_position: 0, y_position: 0, color: 'black')
+    black.pieces.create(type: 'Knight', x_position: 1, y_position: 0, color: 'black')
+    black.pieces.create(type: 'Bishop', x_position: 2, y_position: 0, color: 'black')
+    black.pieces.create(type: 'Queen', x_position: 3, y_position: 0, color: 'black')
   end
 
   def populate_right_black_half!
-    pieces.create(type: 'King', x_position: 4, y_position: 0, color: 'black')
-    pieces.create(type: 'Bishop', x_position: 5, y_position: 0, color: 'black')
-    pieces.create(type: 'Knight', x_position: 6, y_position: 0, color: 'black')
-    pieces.create(type: 'Rook', x_position: 7, y_position: 0, color: 'black')
+    black.pieces.create(type: 'King', x_position: 4, y_position: 0, color: 'black')
+    black.pieces.create(type: 'Bishop', x_position: 5, y_position: 0, color: 'black')
+    black.pieces.create(type: 'Knight', x_position: 6, y_position: 0, color: 'black')
+    black.pieces.create(type: 'Rook', x_position: 7, y_position: 0, color: 'black')
   end
 
   def populate_black_pawns!
     (0..7).each do |i|
-      pieces.create(type: 'Pawn', x_position: i, y_position: 1, color: 'black')
+      black.pieces.create(type: 'Pawn', x_position: i, y_position: 1, color: 'black')
     end
   end
 
   def populate_white_pawns!
     (0..7).each do |j|
-      pieces.create(type: 'Pawn', x_position: j, y_position: 6, color: 'white')
+      white.pieces.create(type: 'Pawn', x_position: j, y_position: 6, color: 'white')
     end
   end
 
   def populate_left_white_half!
-    pieces.create(type: 'Rook', x_position: 0, y_position: 7, color: 'white')
-    pieces.create(type: 'Knight', x_position: 1, y_position: 7, color: 'white')
-    pieces.create(type: 'Bishop', x_position: 2, y_position: 7, color: 'white')
-    pieces.create(type: 'Queen', x_position: 3, y_position: 7, color: 'white')
+    white.pieces.create(type: 'Rook', x_position: 0, y_position: 7, color: 'white')
+    white.pieces.create(type: 'Knight', x_position: 1, y_position: 7, color: 'white')
+    white.pieces.create(type: 'Bishop', x_position: 2, y_position: 7, color: 'white')
+    white.pieces.create(type: 'Queen', x_position: 3, y_position: 7, color: 'white')
   end
 
   def populate_right_white_half!
-    pieces.create(type: 'King', x_position: 4, y_position: 7, color: 'white')
-    pieces.create(type: 'Bishop', x_position: 5, y_position: 7, color: 'white')
-    pieces.create(type: 'Knight', x_position: 6, y_position: 7, color: 'white')
-    pieces.create(type: 'Rook', x_position: 7, y_position: 7, color: 'white')
+    white.pieces.create(type: 'King', x_position: 4, y_position: 7, color: 'white')
+    white.pieces.create(type: 'Bishop', x_position: 5, y_position: 7, color: 'white')
+    white.pieces.create(type: 'Knight', x_position: 6, y_position: 7, color: 'white')
+    white.pieces.create(type: 'Rook', x_position: 7, y_position: 7, color: 'white')
   end
 end
--- db/migrate/20160806190003_change_players_table.rb
+++ db/migrate/20160806190003_change_players_table.rb
@@ -1,5 +1,5 @@
 class ChangePlayersTable < ActiveRecord::Migration[5.0]
-  def change_table :players do |t|
+  change_table :players do |t|
     t.rename :black_or_white, :color
   end
 end
