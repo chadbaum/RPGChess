2722ba478cf3e0ed1162eeb195322645ec2ebfb8
Your Name <zberms@gmail.com>
2016-08-06 20:09:46 +0000

    Switch stats to use devise helper_method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,6 @@ class GamesController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new]
 
   def index
-    # @user = User.find(params[:id])
   end
 
   def create
@@ -13,7 +12,6 @@ class GamesController < ApplicationController
 
   def show
     @piece_positions = Game.find(params[:id]).pieces
-    @user = User.find(params[:id])
   end
 
   def new
