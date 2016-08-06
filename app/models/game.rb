ce033bb647d12318ad1dd06a40c90e3efe2b10d0
Chad Baum <chadbaum@gmail.com>
2016-08-06 23:47:18 +0000

    Refactored populate methods using a helper method to promote DRY

--- app/models/game.rb
+++ app/models/game.rb
@@ -32,42 +32,50 @@ class Game < ActiveRecord::Base
   end
 
   def populate_left_black_half!
-    black.pieces.create(type: 'Rook', x_position: 0, y_position: 0, color: 'black')
-    black.pieces.create(type: 'Knight', x_position: 1, y_position: 0, color: 'black')
-    black.pieces.create(type: 'Bishop', x_position: 2, y_position: 0, color: 'black')
-    black.pieces.create(type: 'Queen', x_position: 3, y_position: 0, color: 'black')
+    create_piece('Rook', 'black', 0, 0)
+    create_piece('Knight', 'black', 1, 0)
+    create_piece('Bishop', 'black', 2, 0)
+    create_piece('Queen', 'black', 3, 0)
   end
 
   def populate_right_black_half!
-    black.pieces.create(type: 'King', x_position: 4, y_position: 0, color: 'black')
-    black.pieces.create(type: 'Bishop', x_position: 5, y_position: 0, color: 'black')
-    black.pieces.create(type: 'Knight', x_position: 6, y_position: 0, color: 'black')
-    black.pieces.create(type: 'Rook', x_position: 7, y_position: 0, color: 'black')
+    create_piece('King', 'black', 4, 0)
+    create_piece('Bishop', 'black', 5, 0)
+    create_piece('Knight', 'black', 6, 0)
+    create_piece('Rook', 'black', 7, 0)
   end
 
   def populate_black_pawns!
     (0..7).each do |i|
-      black.pieces.create(type: 'Pawn', x_position: i, y_position: 1, color: 'black')
+      create_piece('Pawn', 'black', i, 1)
     end
   end
 
   def populate_white_pawns!
-    (0..7).each do |j|
-      white.pieces.create(type: 'Pawn', x_position: j, y_position: 6, color: 'white')
+    (0..7).each do |i|
+      create_piece('Pawn', 'white', i, 6)
     end
   end
 
   def populate_left_white_half!
-    white.pieces.create(type: 'Rook', x_position: 0, y_position: 7, color: 'white')
-    white.pieces.create(type: 'Knight', x_position: 1, y_position: 7, color: 'white')
-    white.pieces.create(type: 'Bishop', x_position: 2, y_position: 7, color: 'white')
-    white.pieces.create(type: 'Queen', x_position: 3, y_position: 7, color: 'white')
+    create_piece('Rook', 'white', 0, 7)
+    create_piece('Knight', 'white', 1, 7)
+    create_piece('Bishop', 'white', 2, 7)
+    create_piece('Queen', 'white', 3, 7)
   end
 
   def populate_right_white_half!
-    white.pieces.create(type: 'King', x_position: 4, y_position: 7, color: 'white')
-    white.pieces.create(type: 'Bishop', x_position: 5, y_position: 7, color: 'white')
-    white.pieces.create(type: 'Knight', x_position: 6, y_position: 7, color: 'white')
-    white.pieces.create(type: 'Rook', x_position: 7, y_position: 7, color: 'white')
+    create_piece('King', 'white', 4, 7)
+    create_piece('Bishop', 'white', 5, 7)
+    create_piece('Knight', 'white', 6, 7)
+    create_piece('Rook', 'white', 7, 7)
+  end
+
+  def create_piece(type, color, x_pos, y_pos)
+    if color == 'white'
+      white.pieces.create(type: type, x_position: x_pos, y_position: y_pos, color: 'white')
+    else
+      black.pieces.create(type: type, x_position: x_pos, y_position: y_pos, color: 'black')
+    end
   end
 end
