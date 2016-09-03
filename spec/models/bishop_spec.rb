d172a61930b6bc43b78489a874d4fe26a6d02a69
Chad Baum <chadbaum@gmail.com>
2016-09-03 17:39:32 +0000

    Updated let variables for bishop rspec

--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -1,24 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Bishop, type: :model do
-  let(:game) { FactoryGirl.create(:game, :populated) }
-  let(:bishop) do
-    game.pieces.find_by(
-      type: 'Bishop',
-      color: 'white',
-      x_position: 2,
-      y_position: 7
-    )
-  end
-  let(:moved_bishop) do
-    game.pieces.create(
-      type: 'Bishop',
-      color: 'white',
-      x_position: 3,
-      y_position: 3,
-      moved: true
-    )
-  end
+  let(:test_game) { FactoryGirl.create(:game, :populated) }
+  let(:bishop) {test_game.white.bishops.first}
 
   describe 'creation validation' do
     it 'should create a white bishop' do
@@ -43,61 +27,69 @@ RSpec.describe Bishop, type: :model do
 
   describe 'invalid moveset' do
     it 'should return false and not update position on invalid move' do
-      expect(moved_bishop.move!(6, 3)).to eq false
-      expect(moved_bishop.x_position).to eq 3
-      expect(moved_bishop.y_position).to eq 3
-      expect(moved_bishop.moved).to eq true
+      bishop.update(x_position: 3, y_position: 3, moved: true)
+      expect(bishop.move!(6, 3)).to eq false
+      expect(bishop.x_position).to eq 3
+      expect(bishop.y_position).to eq 3
+      expect(bishop.moved).to eq true
     end
 
     it 'should return false and not update position on invalid move' do
-      expect(moved_bishop.move!(0, 4)).to eq false
-      expect(moved_bishop.x_position).to eq 3
-      expect(moved_bishop.y_position).to eq 3
-      expect(moved_bishop.moved).to eq true
+      bishop.update(x_position: 3, y_position: 3, moved: true)
+      expect(bishop.move!(0, 4)).to eq false
+      expect(bishop.x_position).to eq 3
+      expect(bishop.y_position).to eq 3
+      expect(bishop.moved).to eq true
     end
 
     it 'should return false and not update position on invalid move' do
-      expect(moved_bishop.move!(6, 5)).to eq false
-      expect(moved_bishop.x_position).to eq 3
-      expect(moved_bishop.y_position).to eq 3
-      expect(moved_bishop.moved).to eq true
+      bishop.update(x_position: 3, y_position: 3, moved: true)
+      expect(bishop.move!(6, 5)).to eq false
+      expect(bishop.x_position).to eq 3
+      expect(bishop.y_position).to eq 3
+      expect(bishop.moved).to eq true
     end
   end
 
   describe 'non-obstructed move' do
     it 'should return true and update position on non-obstructed move' do
-      expect(moved_bishop.move!(5, 5)).to eq true
-      expect(moved_bishop.x_position).to eq 5
-      expect(moved_bishop.y_position).to eq 5
-      expect(moved_bishop.moved).to eq true
+      bishop.update(x_position: 3, y_position: 3, moved: true)
+      expect(bishop.move!(5, 5)).to eq true
+      expect(bishop.x_position).to eq 5
+      expect(bishop.y_position).to eq 5
+      expect(bishop.moved).to eq true
     end
 
     it 'should return true and update position on non-obstructed move' do
-      expect(moved_bishop.move!(2, 2)).to eq true
-      expect(moved_bishop.x_position).to eq 2
-      expect(moved_bishop.y_position).to eq 2
-      expect(moved_bishop.moved).to eq true
+      bishop.update(x_position: 3, y_position: 3, moved: true)
+      expect(bishop.move!(2, 2)).to eq true
+      expect(bishop.x_position).to eq 2
+      expect(bishop.y_position).to eq 2
+      expect(bishop.moved).to eq true
     end
 
     it 'should return true and update position on non-obstructed move' do
-      expect(moved_bishop.move!(5, 5)).to eq true
-      expect(moved_bishop.x_position).to eq 5
-      expect(moved_bishop.y_position).to eq 5
-      expect(moved_bishop.moved).to eq true
+      bishop.update(x_position: 3, y_position: 3, moved: true)
+      expect(bishop.move!(5, 5)).to eq true
+      expect(bishop.x_position).to eq 5
+      expect(bishop.y_position).to eq 5
+      expect(bishop.moved).to eq true
     end
 
     it 'should return true and update position on non-obstructed move' do
-      expect(moved_bishop.move!(2, 4)).to eq true
-      expect(moved_bishop.x_position).to eq 2
-      expect(moved_bishop.y_position).to eq 4
-      expect(moved_bishop.moved).to eq true
+      bishop.update(x_position: 3, y_position: 3, moved: true)
+      expect(bishop.move!(2, 4)).to eq true
+      expect(bishop.x_position).to eq 2
+      expect(bishop.y_position).to eq 4
+      expect(bishop.moved).to eq true
     end
 
     it 'should return true and update position on non-obstructed move' do
-      expect(moved_bishop.move!(4, 2)).to eq true
-      expect(moved_bishop.x_position).to eq 4
-      expect(moved_bishop.y_position).to eq 2
-      expect(moved_bishop.moved).to eq true
+      bishop.update(x_position: 3, y_position: 3, moved: true)
+      expect(bishop.move!(4, 2)).to eq true
+      expect(bishop.x_position).to eq 4
+      expect(bishop.y_position).to eq 2
+      expect(bishop.moved).to eq true
     end
   end
 
@@ -117,17 +109,19 @@ RSpec.describe Bishop, type: :model do
     end
 
     it 'should return false and not update position on obstructed move' do
-      expect(moved_bishop.move!(5, 0)).to eq false
-      expect(moved_bishop.x_position).to eq 3
-      expect(moved_bishop.y_position).to eq 3
-      expect(moved_bishop.moved).to eq true
+      bishop.update(x_position: 3, y_position: 3, moved: true)
+      expect(bishop.move!(5, 0)).to eq false
+      expect(bishop.x_position).to eq 3
+      expect(bishop.y_position).to eq 3
+      expect(bishop.moved).to eq true
     end
 
     it 'should return false and not update position on obstructed move' do
-      expect(moved_bishop.move!(6, 7)).to eq false
-      expect(moved_bishop.x_position).to eq 3
-      expect(moved_bishop.y_position).to eq 3
-      expect(moved_bishop.moved).to eq true
+      bishop.update(x_position: 3, y_position: 3, moved: true)
+      expect(bishop.move!(6, 7)).to eq false
+      expect(bishop.x_position).to eq 3
+      expect(bishop.y_position).to eq 3
+      expect(bishop.moved).to eq true
     end
   end
 end
