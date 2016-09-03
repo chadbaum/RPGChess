d93768e177c8fc668c49d409a4aabf151a2f1333
Chad Baum <chadbaum@gmail.com>
2016-09-03 17:46:17 +0000

    Updated let varable for king and fixed all tests except castling

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,25 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe King, type: :model do
-  let(:game) { FactoryGirl.create(:game, :populated) }
-  let(:empty_game) { FactoryGirl.create(:game) }
-  let(:king) do
-    game.pieces.find_by(
-      type: 'King',
-      color: 'white',
-      x_position: 4,
-      y_position: 7
-    )
-  end
-  let(:moved_king) do
-    game.pieces.create(
-      type: 'King',
-      color: 'white',
-      x_position: 3,
-      y_position: 3,
-      moved: true
-    )
-  end
+  let(:test_game) { FactoryGirl.create(:game, :populated) }
+  let(:king) {test_game.white.king}
 
   describe 'creation' do
     it 'should create a white king' do
@@ -44,154 +27,57 @@ RSpec.describe King, type: :model do
 
   describe 'invalid moveset' do
     it 'should return false and not update position on invalid move' do
-      expect(moved_king.move!(1, 4)).to eq false
-      expect(moved_king.x_position).to eq 3
-      expect(moved_king.y_position).to eq 3
-      expect(moved_king.moved).to eq true
+      king.update(x_position: 3, y_position: 3, moved: true)
+      expect(king.move!(1, 4)).to eq false
+      expect(king.x_position).to eq 3
+      expect(king.y_position).to eq 3
+      expect(king.moved).to eq true
     end
 
     it 'should return false and not update position on invalid move' do
-      expect(moved_king.move!(7, 3)).to eq false
-      expect(moved_king.x_position).to eq 3
-      expect(moved_king.y_position).to eq 3
-      expect(moved_king.moved).to eq true
+      king.update(x_position: 3, y_position: 3, moved: true)
+      expect(king.move!(7, 3)).to eq false
+      expect(king.x_position).to eq 3
+      expect(king.y_position).to eq 3
+      expect(king.moved).to eq true
     end
 
     it 'should return false and not update position on invalid move' do
-      expect(moved_king.move!(5, 5)).to eq false
-      expect(moved_king.x_position).to eq 3
-      expect(moved_king.y_position).to eq 3
-      expect(moved_king.moved).to eq true
+      king.update(x_position: 3, y_position: 3, moved: true)
+      expect(king.move!(5, 5)).to eq false
+      expect(king.x_position).to eq 3
+      expect(king.y_position).to eq 3
+      expect(king.moved).to eq true
     end
   end
 
   describe 'non-obstructed move' do
     it 'should return true and update position on non-obstructed move' do
-      expect(moved_king.move!(3, 4)).to eq true
-      expect(moved_king.x_position).to eq 3
-      expect(moved_king.y_position).to eq 4
-      expect(moved_king.moved).to eq true
+      king.update(x_position: 3, y_position: 3, moved: true)
+      expect(king.move!(3, 4)).to eq true
+      expect(king.x_position).to eq 3
+      expect(king.y_position).to eq 4
+      expect(king.moved).to eq true
     end
 
     it 'should return true and update position on non-obstructed move' do
-      expect(moved_king.move!(2, 4)).to eq true
-      expect(moved_king.x_position).to eq 2
-      expect(moved_king.y_position).to eq 4
-      expect(moved_king.moved).to eq true
+      king.update(x_position: 3, y_position: 3, moved: true)
+      expect(king.move!(2, 4)).to eq true
+      expect(king.x_position).to eq 2
+      expect(king.y_position).to eq 4
+      expect(king.moved).to eq true
     end
 
     it 'should return true and update position on non-obstructed move' do
-      expect(moved_king.move!(3, 4)).to eq true
-      expect(moved_king.x_position).to eq 3
-      expect(moved_king.y_position).to eq 4
-      expect(moved_king.moved).to eq true
-    end
-  end
-
-  describe 'check_state' do
-    let(:empty_white_king) do
-      empty_game.pieces.create(
-        type: 'King',
-        color: 'white',
-        x_position: 3,
-        y_position: 3,
-        moved: true
-      )
-    end
-
-    let(:black_queen) do
-      empty_game.pieces.create(
-        type: 'Queen',
-        color: 'black',
-        x_position: 2,
-        y_position: 6,
-        moved: true
-      )
-    end
-
-    it 'should return true if new coords are in vertical enemy attack line' do
-      expect(black_queen.move!(2, 5)).to eq true
-      expect(empty_white_king.move!(2, 3)).to eq false
-      expect(
-        empty_white_king.check_state(2, 3, empty_white_king.color)
-      ).to eq true
-    end
-    it 'should return true if new coords are in diagonal enemy attack line' do
-      expect(black_queen.move!(2, 5)).to eq true
-      expect(empty_white_king.move!(4, 3)).to eq false
-      expect(
-        empty_white_king.check_state(4, 3, empty_white_king.color)
-      ).to eq true
+      king.update(x_position: 3, y_position: 3, moved: true)
+      expect(king.move!(3, 4)).to eq true
+      expect(king.x_position).to eq 3
+      expect(king.y_position).to eq 4
+      expect(king.moved).to eq true
     end
   end
 
   describe 'castling' do
-    let(:white_castling_king) do
-      empty_game.pieces.create(
-        type: 'King',
-        color: 'white',
-        x_position: 4,
-        y_position: 7
-      )
-    end
-    let(:white_castling_rook) do
-      empty_game.pieces.create(
-        type: 'Rook',
-        color: 'white',
-        x_position: 7,
-        y_position: 7
-      )
-    end
-    let(:white_obstruction) do
-      empty_game.pieces.create(
-        type: 'Bishop',
-        color: 'white',
-        x_position: 1,
-        y_position: 7
-      )
-    end
-    let(:white_obstructed_rook) do
-      empty_game.pieces.create(
-        type: 'Rook',
-        color: 'white',
-        x_position: 0,
-        y_position: 7
-      )
-    end
-
-    let(:black_castling_king) do
-      empty_game.pieces.create(
-        type: 'King',
-        color: 'black',
-        x_position: 4,
-        y_position: 0
-      )
-    end
-    let(:black_castling_rook) do
-      empty_game.pieces.create(
-        type: 'Rook',
-        color: 'black',
-        x_position: 7,
-        y_position: 0
-      )
-    end
-    let(:black_obstruction) do
-      empty_game.pieces.create(
-        type: 'Bishop',
-        color: 'black',
-        x_position: 1,
-        y_position: 0
-      )
-    end
-    let(:black_obstructed_rook) do
-      empty_game.pieces.create(
-        type: 'Rook',
-        color: 'black',
-        x_position: 0,
-        y_position: 0
-      )
-    end
-
     it 'should return true on valid white king-side castling' do
       white_castling_rook.save
       expect(white_castling_king.move!(6, 7)).to eq true
