081216a129eddb9429e41a2ea096d2e47ff131ba
Chad Baum <chadbaum@gmail.com>
2016-09-05 00:39:59 +0000

    Added temporary polling and completed AJAX patch architecture for pieces

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,22 +1,16 @@
 $(function() {
-//     // Picks up all the latest positions from
-//     // the database and updates the view
-//     var $currentPositions = $('#piece_pos').data('positions');
-//
-//     $.each( $currentPositions, function(key, value) {
-//       var currentCell = 'td[data-x="' + value.x_position + '"]' +
-//                         '[data-y="' + value.y_position + '"]';
-//       //formula to map DB piece IDs to the IDs in front view
-//       var pieceIdOnBoard = value.id % 32;
-//       $('#' + pieceIdOnBoard).appendTo(currentCell);
-//     });
+
+  setTimeout(function(){
+    window.location.reload(1);
+  }, 15000);
+
   $('#chess-board td span').draggable({
     containment: "#chess-board",
     cursor: "all-scroll",
     opacity: 0.6,
     revert: 'invalid',
     start: function ( event, ui ) {
-
+      //PUT AVAILABLE MOVES HERE
     }
   });
 
@@ -41,12 +35,12 @@ $(function() {
         url: '/pieces/' + $(draggedPiece).data('id'),
         dataType: 'json',
         data: {
-          id: $(event.target).data('id'),
           x: $(event.target).data('x'),
           y: $(event.target).data('y')
         }
       })
       draggedPiece.detach().css({top: 0, left: 0}).appendTo(targetTile);
+      location.reload();
     }
   });
 });
--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,18 +1,10 @@
 class PiecesController < ApplicationController
   respond_to :json
-  
+
   def move
     @piece = Piece.find(params[:id])
     x = params[:x].to_i
     y = params[:y].to_i
     @piece.move!(x, y)
-    respond_with @piece
-
-    # private
-    #
-    # def piece_params
-    #   params.require(:piece).permit(:x_position, :y_position, :moved)
-    # end
-
   end
 end
